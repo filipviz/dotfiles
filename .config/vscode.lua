5ab241f48dccc7f57384b1b32375576b3920b8e7
Filip <f@filip.world>
2024-12-28 15:42:56 -0500

    vscode dotfiles

---
+++ .config/vscode.lua
@@ -0,0 +1,25 @@
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Remove search highlighting on insert mode
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*",
+	command = "set nohlsearch",
+})
+
+-- Enable search highlighting when entering command-line mode for search commands
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+	pattern = { "/", "?" },
+	command = "set hlsearch",
+})
+
+local lazypath = "/Users/f/.local/share/lunarvim/site/pack/lazy/opt/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+})
